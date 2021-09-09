5978d703cf9a6a61b0ae2ed07b69bf07e047b6fa
sam0811956 <sam0811956@gmail.com>
2021-09-09 03:58:16 +0000

    Minor TimeZone

--- mask_api/models.py
+++ mask_api/models.py
@@ -44,3 +44,21 @@ class OpeningHour(models.Model):
             self.week_day, self.start_hour, self.start_min, self.end_hour, self.end_min
         )
     
+class User(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    cashbalance = models.FloatField(null=False)
+
+    def __str__(self):
+        return '{0} (cashbalance: {1})'.format(self.name, self.cashbalance)
+
+class PurchaseHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='purchasehistory', null=True, blank=True)
+    pharmacies_name = models.CharField(max_length=100, null=False)
+    mask_name = models.CharField(max_length=100, null=False)
+    trans_amount = models.FloatField(null=False)
+    trans_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return 'pharmacies_name: {0}, mask_name: {1}, (amount: {2}, date: {3})'.format(
+            self.pharmacies_name, self.mask_name, self.trans_amount,
+            self.trans_date)
