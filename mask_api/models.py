9a10873e3422765f4cf873df921f0447d3a8b9f8
sam0811956 <32931993+sam0811956@users.noreply.github.com>
2021-09-27 14:01:08 +0800

    Update models.py

--- mask_api/models.py
+++ mask_api/models.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.search import SearchVectorField
 
 class PharMacies(models.Model):
     name = models.CharField(max_length=100, null=False)
-    cashbalance = models.FloatField(null=False)
+    cash_balance = models.FloatField(null=False)
 
     def __str__(self):
         return '{0} (cash: {1})'.format(self.name, self.cashbalance)
